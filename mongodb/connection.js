0152c2ded8e37247bbe91be6ee94d27fd823d1cf
Munazar-99 <munazar.ali.001@gmail.com>
2023-08-08 23:46:26 +0200

    refactor and add comments in the database connection  file

--- mongodb/connection.js
+++ mongodb/connection.js
@@ -1,34 +1,48 @@
+// Import required modules
 import { MongoClient } from 'mongodb';
 import * as dotenv from 'dotenv';
 import { v4 as uuidv4 } from 'uuid';
 
-dotenv.config()
+// Load environment variables from .env file
+dotenv.config();
 
-const url = process.env.URL
+// Set MongoDB connection URL and database name
+const url = process.env.URL;
 const dbName = 'DAL-E';
 
 let client;
 let carts;
 
+// Function to open MongoDB connection
 const openConnection = async () => {
+  // Connect to MongoDB using the provided URL
   client = await MongoClient.connect(url, { useUnifiedTopology: true });
+  // Access the specified database
   const db = client.db(dbName);
+  // Access the 'Images' collection within the database
   carts = db.collection('Images');
 };
 
+// Function to generate a unique ID for a post
 const generateCartId = () => uuidv4();
 
+// Exported function to create a new post
 export const createNewPost = async (name, prompt, photoUrl) => {
   try {
+    // Generate a unique post ID
     const generatedId = generateCartId();
+    // Open a connection to the MongoDB server
     await openConnection();
+    // Insert the new post data into the 'Images' collection
     await carts.insertOne({
       postId: generatedId,
       name: name,
       prompt: prompt,
       photo: photoUrl.url,
     });
+    // Find the newly inserted post in the collection
     const result = await carts.find({ cartId: generatedId }).toArray();
+    // Close the MongoDB connection after 1 second
     setTimeout(() => client.close(), 1000);
     return result[0];
   } catch (e) {
@@ -36,14 +50,17 @@ export const createNewPost = async (name, prompt, photoUrl) => {
   }
 };
 
+// Exported function to retrieve all posts
 export const getAllPost = async () => {
   try {
+    // Open a connection to the MongoDB server
     await openConnection();
+    // Retrieve all posts from the 'Images' collection
     const results = await carts.find().toArray();
+    // Close the MongoDB connection after 1 second
     setTimeout(() => client.close(), 1000);
     return results;
   } catch (e) {
     throw new Error(e.message);
   }
 };
-
